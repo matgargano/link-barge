6f97fec4de58b485831fd50fe12fbd0cc3189f1e
Mat Gargano <mat@statenweb.com>
2023-04-20 07:17:55 -0400

    initial commit, post class 1

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import { cache } from "react";
+import Dummy from "./components/Dummy";
+import { data } from "./utils/dummy";
+
+export default async function Home() {
+  return (
+    <main>
+      {/* <TopContent /> */}
+      {/* <SocialLinks /> */}
+      {/* <NonSocialLinks /> */}
+    </main>
+  );
+}
