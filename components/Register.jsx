8777c5ac53217069f48d487655e5d35c08fdf3a0
Mat Gargano <mat@statenweb.com>
2023-05-03 18:20:17 -0400

    create register form, add additional button class

--- components/Register.jsx
+++ components/Register.jsx
@@ -1,29 +1,99 @@
 "use client";
 
 import { registerUser } from "csc-start/utils/data";
-import { useState } from "react";
+import { useReducer } from "react";
 
 const Register = () => {
-
-    const [response, setResponse] = useState(null);
-
-    const testRegister = async () => {
-        const register = await 
-            registerUser('foo32@bar.com', '123456!', 'aaJohn Doe', 'fa-john-doe')
-
-        setResponse(register);
+  function reducer(state, action) {
+    switch (action.type) {
+      case "email":
+      case "name":
+      case "slug":
+      case "password":
+        return { ...state, [action.type]: action.value };
+      case "loading":
+        return { ...state, loading: action.loading };
+      case "response":
+        return { ...state, response: action.response };
     }
-    return <div>
-        {response && !response.success && <div className="bg-red-500">
-            {response.message}
-            </div>}
-    <button onClick={testRegister}>
-        Click me
-    </button>
-    <pre>{JSON.stringify(response,0,1)}</pre>
+
+    throw Error("Unknown action.");
+  }
+
+  const initialState = {
+    email: "",
+    name: "",
+    slug: "",
+    password: "",
+    response: "",
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { email, name, slug, password, response, loading } = state;
+  const register = async (e) => {
+    dispatch({ type: "loading", loading: true });
+    e.preventDefault();
+
+    const response = await registerUser(email, password, name, slug);
+    dispatch({ type: "response", response });
+    dispatch({ type: "loading", loading: false });
+  };
+  return (
+    <div className="barge">
+      {response && (
+        <div
+          className={`${
+            response.success
+              ? "bg-green-200 border-2 border-green-800 text-green-800"
+              : "bg-red-200 border-2 border-red-800 text-red-800"
+          } py-2 px-5 my-10 text-center`}
+        >
+          <span className="font-bold">
+            {response.success
+              ? `Success ${response.message ? `: ` : ``}`
+              : "Failure: "}
+          </span>
+          {response.message}
+        </div>
+      )}
+      <h2 className="my-10 h1 text-center">Register</h2>
+      <form
+        onSubmit={register}
+        className={loading ? "opacity-[10%] pointer-events-none" : ""}
+      >
+        {Object.keys(initialState)
+          .filter((k) => !["response", "loading"].includes(k))
+          .map((key) => {
+            let type = "text";
+            if ([type].includes("email", "password")) {
+              type = key;
+            }
+
+            return (
+              <p key={key} className="mb-5">
+                <label className="h3 capitalize w-[75px] inline-block">
+                  {key}*
+                </label>
+                <input
+                  className="h3 border-2 border-black ml-5 inline-block w-[220px] px-2"
+                  required
+                  name={key}
+                  onChange={(e) =>
+                    dispatch({ type: e.target.name, value: e.target.value })
+                  }
+                  value={state[key]}
+                  type={key}
+                />
+              </p>
+            );
+          })}
+        <div className="flex justify-center my-10">
+          <input className="button small" type="submit"></input>
+        </div>
+      </form>
     </div>
-
-}
-
+  );
+};
 
 export default Register;
